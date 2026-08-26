abe0b58fc04d96b26fe750c0f8affb6cd6b0a549
Mutsumi Yamamoto <mutsu224@gmail.com>
2026-08-26 13:38:46 +0900

    Add files via upload
    HelloWrorldの基本的なコード

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+//-------------------
+//Hello World: "Hello, World!" を標準出力に表示するプログラムを作成してください。
+//-------------------
+int main(){
+    std::cout << "Hello World!" ;
+}
+
+#if 0
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+#endif
