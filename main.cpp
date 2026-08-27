3188371304447b84b38b3b3cfbea56780cd5adb2
Mutsumi Yamamoto <mutsu224@gmail.com>
2026-08-27 10:13:02 +0900

    add display day & time

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 
 using namespace std;
 //-------------------
 //Hello World: "Hello, World!" を標準出力に表示するプログラムを作成してください。
 //-------------------
 int main(){
-    std::cout << "Hello World! Everyone!!!!!!" ;
+    std::cout << "Hello World! Everyone!!!!!!" << std::endl;
+
+    auto now = std::chrono::system_clock::now();     // 現在のシステム時刻を取得
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now); // time_t型に変換
+    std::tm* local_tm = std::localtime(&now_c);           // ローカル時間に変換
+    std::cout << "現在の時刻: " << std::put_time(local_tm, "%Y-%m-%d %H:%M:%S") << std::endl;
+
 }
+
+
+
+
+
+
+
+
+
+
 
 #if 0
 #include <iostream>
